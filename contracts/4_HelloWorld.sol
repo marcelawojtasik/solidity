6c8c161140062d19a1dd148b1a51b7a1ee72d72d
marcelawojtasik <marcelawojtasik@gmail.com>
2024-10-24 01:13:57 -0300

    HelloWorldSolidity

---
+++ contracts/4_HelloWorld.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract HelloWorld{
+    string name;
+    address wallet;
+    string message;
+    uint256 date;
+
+//sin parámetros - con atributos
+    function seeLastVisit() external view returns (string memory, address, string memory, uint256){
+        return (name, wallet, message, date); 
+    }
+
+//con parámetros 
+    function writeVisit(string memory _name, string memory _message) external {
+        name = _name;
+        wallet = msg.sender; //pido dir wallet del que envia
+        message = _message;
+        date = block.timestamp; //pido fecha a solidity
+    }
+}
