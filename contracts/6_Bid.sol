fa1954c085298bf1bb494997d666c3bbcb550a96
marcelawojtasik <marcelawojtasik@gmail.com>
2024-10-26 01:10:13 -0300

    bid value validation

--- contracts/6_Bid.sol
+++ contracts/6_Bid.sol
@@ -5,12 +5,25 @@ pragma solidity >=0.8.2 <0.9.0;
 contract Bid{
     
     uint256 finishTimestamp;
+    uint256 public maxBid; //para poder verla
+    address maxBidder;
 
     constructor(uint256 durationInSeconds){
         finishTimestamp = block.timestamp + durationInSeconds;
     }
 
-    function makeBid() view external {
+    function makeBid() external payable {
         require(block.timestamp < finishTimestamp, "The bid has finished");
-    }
+
+        if (msg.value > maxBid){
+            maxBid = msg.value;
+            maxBidder = msg.sender;
+        }
+        else{
+            revert("The bid is not enough"); //devuelve los fondos
+        }
+    } 
+    //require es como usar if... else
+    //payable modificador q indica q function acepta pagos
+    //msg.value tiene el valor q voy a mandar
 }
